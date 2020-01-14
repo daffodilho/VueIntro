5b25eed75a8b7e58925eecc561c58bd034e2acf1
daffodilho <daffodilho@gmail.com>
2020-01-14 17:38:15 -0500

    adding posters and videos

--- js/main.js
+++ js/main.js
@@ -19,6 +19,10 @@ var vm = new Vue({
       { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
     ],
 
+    videotitle: "video title goes here",
+    videodescription: "vid description goes here",
+    videosource: "",
+
     showDetails: false
   },
 
@@ -38,6 +42,17 @@ var vm = new Vue({
       console.log('set user prefs via routing and probably a component');
 
 
+    },
+
+    // this is ES6 data destructuring - pull the keys and values you need, not the whole object
+    loadMovie({name, description, vidsource}) {
+      console.log('show movie details');
+
+      this.videotitle = name;
+      this.videodescription = description;
+      this.videosource = vidsource;
+
+      this.showDetails = true;
     }
   }
 });
